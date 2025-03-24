7bd3e1ce495e7597ecb82fc82ffda3b8badfb6b7
Aziz <azooz-550@hotmail.com>
2025-03-24 21:05:56 +0300

    (feat): add new pages for voluntering hours logs and member management

---
+++ src/pages/MemberManagement.jsx
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout';
+import { Users } from 'lucide-react';
+
+const MemberManagement = () => {
+  return (
+    <Layout>
+      {/* TODO: Add content here */}
+    </Layout>
+  );
+};
+
+export default MemberManagement; 
